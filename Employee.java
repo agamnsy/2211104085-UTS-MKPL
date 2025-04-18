ece91443384818619af15de9b0f9e0304b0c7774
agam <agamnasy28@gmail.com>
2025-04-18 16:18:00 +0700

    Refactor : Mengganti tipe data atribut gender dari boolean menjadi enum Gender (Male, Female)

--- Employee.java
+++ Employee.java
@@ -4,6 +4,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
+	
+	public enum Gender {
+		Male, Female
+	}
 
 	private String employeeId;
 	private String firstName;
@@ -17,7 +21,7 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+	private Gender gender; //true = Laki-laki, false = Perempuan (DONE)
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -29,7 +33,7 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
